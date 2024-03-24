3fea91263691639393900773fd869ed30ddcce6d
anthonydinino <adinino1996@gmail.com>
2024-03-24 16:20:14 +1030

    remove debug

--- app.py
+++ app.py
@@ -3,10 +3,7 @@ from scraper import scrape
 
 app = Flask(__name__)
 
-@app.route("/scrape")
+@app.route("/")
 def data():
   content = scrape(request.args.get('query'))
   return render_template("scrape.html", content=content)
-
-if __name__ == "__main__":
-  app.run(debug=True)
